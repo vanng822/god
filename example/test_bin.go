f800765d2c28870b9f35b1a05d47a86edd891d02
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-31 13:53:18 +0100

    No need of format printing

--- example/test_bin.go
+++ example/test_bin.go
@@ -11,12 +11,12 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", "World")
+		fmt.Fprint(w, "Hello World")
 	})
 
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, os.Kill, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
-	go http.ListenAndServe(":8081", nil)
+	go http.ListenAndServe(":8080", nil)
 	sig := <-sigc
 	log.Printf("Got signal: %s", sig)
 }
